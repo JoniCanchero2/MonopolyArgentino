16b20d5b41c265dd71ec10e01a41d2f346d524d4
JoniCanchero2 <jonathanramirez1605@gmail.com>
2025-10-21 11:06:29 -0300

    Add files via upload
    Base para iniciar el codeo

---
+++ objetos.py
@@ -0,0 +1,26 @@
+def casilla():
+  def __init__(self, id_propuedad, nombre, color, propietario,valor_propiedad, valor_alquiler, cantidad_casas):
+    self.id_propuedad = id_propuedad
+    self.nombre = nombre
+    self.color = color
+    self.propietario = propietario
+    self.valor_propiedad = valor_propiedad
+    self.valor_alquiler = valor_alquiler
+    self.cantidad_casas = cantidad_casas
+
+
+def jugador():
+  def __init__(self, id_jugador, dinero, posicion, propiedades_compradas):
+    self.id_jugador = id_jugador
+    self.dinero = dinero
+    self.posicion = posicion
+    self.propiedades_compradas = propiedades_compradas
+
+def tablero(): 
+  def __init__(self, id_propiedad, nombre):
+    self.id_propiedad = id_propiedad
+    self.nombre = nombre
+
+  def mover_jugador(self, posicion, valor_dados):
+    nueva_posicion = (posicion + valor_dados)
+    return nueva_posicion
